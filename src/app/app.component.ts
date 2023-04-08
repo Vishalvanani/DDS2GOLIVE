bb86fe8c821a9e95419d78a485d37c5e8d593013
sandip moradiya <sandipmoradiya1996@gmail.com>
2023-04-08 14:36:12 +0530

    - Added Idle Time out feature

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { Device } from '@capacitor/device';
 import { SharedService } from './services/shared.service';
+import { AlertController, ModalController } from '@ionic/angular';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor(private shared:SharedService) {
+  idleLogoutTimer: any;
+  constructor(
+    private shared:SharedService,
+    private alertController:AlertController,
+    private modalCtrl:ModalController,
+    private router: Router
+    ) {
     const logDeviceInfo = async () => {
       //const info = await Device.getInfo();
     const deviceid = await Device.getId();
@@ -16,5 +24,58 @@ export class AppComponent {
     };
 
     logDeviceInfo();
+  }
+
+
+  ngOnInit() {
+    this.isUserExist();
+  }
+
+  restartIdleLogoutTimer() {
+    clearTimeout(this.idleLogoutTimer);
+    this.idleLogoutTimer = setTimeout(() => {
+      this.logoutUser();
+    }, 60000);
+  }
+  
+  async logoutUser() {
+    // your logout logic here
+    let idealTimeoutAlert = await this.alertController.create({
+      header: 'Attention!',
+      message: 'Your session is expired',
+      backdropDismiss: false,
+      buttons: [
+        {
+          text: 'Login',
+          role: 'cancel',
+          handler: () => {
+            // Handle Logout Code
+            this.modalCtrl.dismiss();
+            this.router.navigate(['login']);
+          },
+        },
+        {
+          text: 'Exit',
+          handler: () => {
+            // Handle Exit Code
+            (navigator as any).app.exitApp();
+          },
+        },
+      ],
+    });
+    idealTimeoutAlert.present();
+  }
+
+  @HostListener('touchstart')
+  onTouchStart() {
+    this.isUserExist();
+  }
+
+  isUserExist(){
+    let isUserExists: any = localStorage.getItem('user') ? localStorage.getItem('user') : '';
+    if (isUserExists) {
+      this.restartIdleLogoutTimer();
+    }
+
   }
 }
