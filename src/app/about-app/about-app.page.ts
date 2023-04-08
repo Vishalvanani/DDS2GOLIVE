da33e9afb5d07efaeb59570931de1c73cceea154
sandip moradiya <sandipmoradiya1996@gmail.com>
2023-04-09 00:45:52 +0530

    - App version added

--- src/app/about-app/about-app.page.ts
+++ src/app/about-app/about-app.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { App } from '@capacitor/app';
 
 @Component({
   selector: 'app-about-app',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about-app.page.scss'],
 })
 export class AboutAppPage implements OnInit {
+  appVersion: any;
 
   constructor() { }
 
   ngOnInit() {
+    App.getInfo().then((res:any) => {
+      this.appVersion = res.version;
+    });
   }
 
 }
